fca14537aed67ed07ce655f746d25bbfa657717c
VotreNom <VotreEmail>
2025-12-04 09:49:08 +0100

    ex00 et 01 finit(juste repasser un coup dessus plus tard)

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <iostream>
 #include <cctype>
+#include <limits>
 
 static bool char_status = 0;
 static bool char_status_display = 0;
@@ -15,6 +16,8 @@ static bool double_status = 0;
 
 void printAllType(char c, int i, float f, double d)
 {
+    std::cout << std::fixed << std::setprecision(1);
+
     std::cout << "char: ";
     if (char_status == 1)
         std::cout << "impossible" << std::endl;
@@ -22,171 +25,85 @@ void printAllType(char c, int i, float f, double d)
         std::cout << "Non displayable" << std::endl;
     else
         std::cout << "\'" << c << "\'" << std::endl;
+
     std::cout << "int: ";
     if (int_status == 1)
         std::cout << "impossible" << std::endl;
     else
         std::cout << i << std::endl;
+
     std::cout << "float: ";
     if (float_status == 1)
         std::cout << "impossible" << std::endl;
+    else if (std::isnan(f))
+        std::cout << "nanf" << std::endl;
+    else if (std::isinf(f))
+        std::cout << (f > 0 ? "+inff" : "-inff") << std::endl;
     else
         std::cout << f << "f" << std::endl;
+
     std::cout << "double: ";
     if (double_status == 1)
         std::cout << "impossible" << std::endl;
+    else if (std::isnan(d))
+        std::cout << "nan" << std::endl;
+    else if (std::isinf(d))
+        std::cout << (d > 0 ? "+inf" : "-inf") << std::endl;
     else
         std::cout << d << std::endl;
 }
 
 void convertFromChar(const std::string &str)
 {
-    char c = str[1];
+    char c;
+        c = str[1];
+    else
+        c = str[0];
+
     int i = static_cast<int>(c);
     float f = static_cast<float>(c);
     double d = static_cast<double>(c);
+    
     if (!std::isprint(c))
         char_status_display = 1;     
+        
     printAllType(c, i, f, d);
 }
-
-// void convertFromInt(const std::string &str)
-// {
-//     errno = 0; 
-//     char *endptr = NULL;
-//     char c = '\0';
-//     float f = 0.0f;
-//     double d = 0.0;
-//     long l = std::strtol(str.c_str(), &endptr, 10);
-//     int i = static_cast<int>(l);
-//     if (errno == ERANGE || *endptr != '\0' || l > INT_MAX || l < INT_MIN)
-//     {
-//         int_status = 1;
-//         char_status = 1;
-//         float_status = 1;
-//         double_status = 1;
-//     }
-//     if (int_status == 0)
-//     {
-//         if (l > CHAR_MAX || l < CHAR_MIN)
-//             char_status = 1;
-//         else
-//         {
-//             c = static_cast<char>(l);
-//             if (!std::isprint(c))
-//                 char_status_display = 1;
-//         }
-//         f = static_cast<float>(i);
-//         d = static_cast<double>(i);
-//     }
-//     printAllType(c, i, f, d);
-// }
-
-// void convertFromFloat(const std::string &str)
-// {
-//     errno = 0; 
-//     char *endptr = NULL;
-//     char c = '\0';
-//     int i = 0;
-//     double d = 0.0;
-//     float f = std::strtof(str.c_str(), &endptr);
-//     if (errno == ERANGE || *endptr != '\0')
-//     {
-//         int_status = 1;
-//         char_status = 1;
-//         float_status = 1;
-//         double_status = 1;
-//     }
-//     if (std::isnan(f) || std::isinf(f))
-//     {
-//         int_status = 1;
-//         char_status = 1;
-//     }
-//     if (int_status == 0)
-//     {
-//         if (f > INT_MAX || f < INT_MIN)
-//             int_status = 1;
-//         else
-//             i = static_cast<int>(f);
-//         if (f > CHAR_MAX || f < CHAR_MIN)
-//             char_status = 1;
-//         else
-//         {
-//             c = static_cast<char>(f);
-//             if (!std::isprint(c))
-//                 char_status_display = 1;
-//         }
-//     }
-//     d = static_cast<double>(f);
-    
-//     printAllType(c, i, f, d);
-// }
-
-// void convertFromDouble(const std::string &str)
-// {
-//     errno = 0; 
-//     char *endptr = NULL;
-//     char c = '\0';
-//     int i = 0;
-//     float f = 0.0f;
-//     double d = std::strtod(str.c_str(), &endptr);
-//     if (errno == ERANGE || *endptr != '\0')
-//     {
-//         int_status = 1;
-//         char_status = 1;
-//         float_status = 1;
-//         double_status = 1;
-//     }
-//     if (std::isnan(d) || std::isinf(d))
-//     {
-//         int_status = 1;
-//         char_status = 1;
-//     }
-//     if (int_status == 0)
-//     {
-//         if (d > INT_MAX || d < INT_MIN)
-//             int_status = 1;
-//         else
-//             i = static_cast<int>(d);
-//         if (d > CHAR_MAX || d < CHAR_MIN)
-//             char_status = 1;
-//         else
-//         {
-//             c = static_cast<char>(d);
-//             if (!std::isprint(c))
-//                 char_status_display = 1;
-//         }
-//     }
-//     f = static_cast<float>(d);
-//     float_status = 1;
-//     printAllType(c, i, f, d);
-// }
 
 void convertFromInt(const std::string &str)
 {
     errno = 0; 
     char *endptr = NULL;
     char c = '\0';
-    double d = 0.0;
     float f = 0.0f;
-    int i = std::strtol(str.c_str(), &endptr, 10);
-    if (i == -1)
-            int_status = 1;
-    if(i < 32 || i > 127)
+    double d = 0.0;
+    
+    long l = strtol(str.c_str(), &endptr, 10);
+    int i = static_cast<int>(l);
+
+    if (errno == ERANGE || *endptr != '\0' || l > std::numeric_limits<int>::max() || l < std::numeric_limits<int>::min())
     {
-        if (i >= 0 && i <= 32)
-            char_status_display = 1;
-        else
-            char_status = 1;
+        int_status = 1;
+        char_status = 1;
+        float_status = 1;
+        double_status = 1;
     }
-    else
-        c = static_cast<char>(i);
-    f = static_cast<float>(i);
-    if (f == -1)
-            int_status = 1;
-    d = static_cast<double>(i);
-    if (d == -1)
-            int_status = 1;
+    
+    if (int_status == 0)
+    {
+        if (l > std::numeric_limits<char>::max() || l < std::numeric_limits<char>::min())
+            char_status = 1;
+        else
+        {
+            c = static_cast<char>(l);
+            if (!std::isprint(c))
+                char_status_display = 1;
+        }
+
+        f = static_cast<float>(i);
+        d = static_cast<double>(i);
+    }
+    
     printAllType(c, i, f, d);
 }
 
@@ -197,31 +114,42 @@ void convertFromFloat(const std::string &str)
     char c = '\0';
     int i = 0;
     double d = 0.0;
-    float f = std::strtof(str.c_str(), &endptr);
-    if (f == -1)
-            int_status = 1;
-    if (str == "nanf" || str == "+inff" || str == "-inff")
+    
+    float f = strtof(str.c_str(), &endptr);
+
+    if (errno == ERANGE || *endptr != '\0')
+    {
+        int_status = 1;
+        char_status = 1;
+        float_status = 1;
+        double_status = 1;
+    }
+    
+    if (std::isnan(f) || std::isinf(f))
     {
         int_status = 1;
         char_status = 1;
     }
-    else
+    
+    if (int_status == 0)
     {
-        c = static_cast<char>(f);
-        i = static_cast<int>(f);
-        if (i == -1)
+        if (f > std::numeric_limits<int>::max() || f < std::numeric_limits<int>::min())
             int_status = 1;
-    }
-    if(i < 32 || i > 127)
-    {
-        if (i >= 0 && i <= 32)
-            char_status_display = 1;
         else
+            i = static_cast<int>(f);
+            
+        if (f > std::numeric_limits<char>::max() || f < std::numeric_limits<char>::min())
             char_status = 1;
+        else
+        {
+            c = static_cast<char>(f);
+            if (!std::isprint(c))
+                char_status_display = 1;
+        }
     }
+    
     d = static_cast<double>(f);
-    if (d == -1)
-            int_status = 1;
+    
     printAllType(c, i, f, d);
 }
 
@@ -232,36 +160,56 @@ void convertFromDouble(const std::string &str)
     char c = '\0';
     int i = 0;
     float f = 0.0f;
-    double d = std::strtod(str.c_str(), &endptr);
-    if (d == -1)
-            int_status = 1;
-    if (str == "nan" || str == "+inf" || str == "-inf")
+    
+    double d = strtod(str.c_str(), &endptr);
+
+    if (errno == ERANGE || *endptr != '\0')
+    {
+        int_status = 1;
+        char_status = 1;
+        float_status = 1;
+        double_status = 1;
+    }
+    
+    if (std::isnan(d) || std::isinf(d))
     {
         int_status = 1;
         char_status = 1;
     }
-    else
+
+    if (int_status == 0)
     {
-        c = static_cast<char>(d);
-        i = static_cast<int>(d);
-        if (i == -1)
+        if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min())
             int_status = 1;
-    }
-    if(i < 32 || i > 127)
-    {
-        if (i >= 0 && i <= 32)
-            char_status_display = 1;
         else
+            i = static_cast<int>(d);
+            
+        if (d > std::numeric_limits<char>::max() || d < std::numeric_limits<char>::min())
             char_status = 1;
+        else
+        {
+            c = static_cast<char>(d);
+            if (!std::isprint(c))
+                char_status_display = 1;
+        }
     }
+
     f = static_cast<float>(d);
-    if (f == -1)
-            int_status = 1;
+    
+    if (!std::isinf(d) && (d > std::numeric_limits<float>::max() || d < -std::numeric_limits<float>::max()))
+        float_status = 1;
+    
     printAllType(c, i, f, d);
 }
 
 void ScalarConvert::convert(const std::string &str)
 {
+    char_status = 0;
+    char_status_display = 0;
+    int_status = 0;
+    float_status = 0;
+    double_status = 0;
+    
     type type = FindType(str);
 
     switch (type)
@@ -279,13 +227,21 @@ void ScalarConvert::convert(const std::string &str)
             convertFromDouble(str);
             break;
         default:
-            std::cout << "Invalid input." << std::endl;
+            std::cout << "char: impossible" << std::endl;
+            std::cout << "int: impossible" << std::endl;
+            std::cout << "float: impossible" << std::endl;
+            std::cout << "double: impossible" << std::endl;
             break;
     }
 }
 
 type ScalarConvert::FindType(const std::string &str)
 {
+    if (str.length() == 1)
+    {
+        if (std::isprint(str[0]) && !std::isdigit(str[0]))
+            return (CHAR);
+    }
     if (str.length() == 3 && str[0] == '\'' && str[2] == '\'')
         return (CHAR);
     if (str == "nan" || str == "+inf" || str == "-inf")
@@ -343,5 +299,3 @@ type ScalarConvert::FindType(const std::string &str)
         return (INT);
     }
 }
-//atof pour double et float
-//atoi pour int
